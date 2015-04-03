8f57c6796696f88111c3f6200c18e98c8e123889
Frans Peter Larsen <larsen.frans@gmail.com>
2015-04-03 22:22:32 +0200

    Simple peel detection WITH FANCY GIZMOS

--- Assets/Scripts/RightHand.cs
+++ Assets/Scripts/RightHand.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections; 
 
 public class RightHand : MonoBehaviour {
+	public Vector3 PeelingLocation;
+	public float PeelingRadius;
+
 	// Use this for initialization
 	void Start () {
 		Cursor.lockState = CursorLockMode.Locked;
@@ -10,14 +13,39 @@ public class RightHand : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		MoveHand ();
+
+		if (Input.GetKey (KeyCode.Mouse0)) 
+			PeelPeelables ();
+	}
+
+	void PeelPeelables ()
+	{
+		Collider[] collidedObjects = Physics.OverlapSphere (PeelingLocation + transform.position, PeelingRadius);
+
+
+		foreach (Collider obj in collidedObjects) {
+			if(obj.CompareTag("Peelable")) {
+				obj.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+	
+			}
+		}
 	}
 
 	void MoveHand() {
-		Vector3 mouseMovement = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0f);
+		Vector3 mouseMovement = new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0f);
 
 		Vector3 currentPos = transform.position;
-		currentPos.x += mouseMovement.y;
-		currentPos.z += -mouseMovement.x;
+		currentPos.x += mouseMovement.y * Time.deltaTime * 50;
+		currentPos.z += mouseMovement.x * Time.deltaTime * 50;
+
 		transform.position = currentPos;
+	}
+
+	void OnDrawGizmos() {
+		Color c = Gizmos.color;
+		c.a = 0.65f;
+		Gizmos.color = c;
+
+		Gizmos.DrawSphere (PeelingLocation + transform.position, PeelingRadius);
 	}
 }
