add3460cdfee7933a4814285f13e9b3ab0c02b6c
Simon Ingemann Christensen <ingemannofdoom@gmail.com>
2015-04-03 22:06:51 +0200

    Merge

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,17 +3,12 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour {
 
-<<<<<<< HEAD
-public GameObject [] model;
-//public Vector3 startPosition;
-
-=======
 public GameObject [] models;
->>>>>>> Making timer to work MainGameManager and coroutines :v
+
 	// Use this for initialization
 	void Start () {
 		transform.Translate (-5f * Time.deltaTime, 0f, 0f);
-		rigidbody.AddForce(acceleration, 0f, 0f, ForceMode.Acceleration);
+		GetComponent<Rigidbody>().AddForce(acceleration, 0f, 0f, ForceMode.Acceleration);
 
 	
 	}
