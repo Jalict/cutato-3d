6f1940cc0de5687e206160de6121283779f4a5ba
Frans Peter Larsen <larsen.frans@gmail.com>
2015-04-03 21:10:34 +0200

    Simple RightHand movement

---
+++ Assets/Scripts/RightHand.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RightHand : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveHand ();
+	}
+
+	void MoveHand() {
+		Vector3 mouseMovement = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0f);
+
+		Vector3 currentPos = transform.position;
+		currentPos.x += mouseMovement.y;
+		currentPos.z += -mouseMovement.x;
+		transform.position = currentPos;
+	}
+}
