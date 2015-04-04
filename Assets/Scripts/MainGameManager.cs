434eb56702fe439b0eafacdd33fb11e214be95ba
Simon Ingemann Christensen <ingemannofdoom@gmail.com>
2015-04-04 16:33:34 +0200

    make variables public

--- Assets/Scripts/MainGameManager.cs
+++ Assets/Scripts/MainGameManager.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class MainGameManager : MonoBehaviour {
 
 	public static MainGameManager instance;
-	private int score;
-	private ArrayList peeledPotatoes = new ArrayList();
-	private float timeRemaining = 5; 
+	public int score;
+	public ArrayList peeledPotatoes = new ArrayList();
+	public float timeRemaining = 5; 
 	private IEnumerator countdown;
 	private bool isCountingDown;
 
@@ -51,23 +51,5 @@ public class MainGameManager : MonoBehaviour {
 		}
 		Debug.Log ("Time's up!");
 		Application.LoadLevel (0);
-	}
-
-	public int Score {
-		get {
-			return score;
-		}
-	}
-
-	public ArrayList PeeledPotatoes {
-		get {
-			return peeledPotatoes;
-		}
-	}
-
-	public float TimeRemaining {
-		get {
-			return timeRemaining;
-		}
 	}
 }
