2728e94e57e3dbf0364554a913459c84db5424ec
Maja Daniela Hansen <MajaDanielaHansen@Gmail.com>
2015-04-04 23:11:07 +0200

    Clock prefab. potato moving with mouse position and being pushed when mouse click.

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -7,20 +7,43 @@ public class StartGame : MonoBehaviour {
 	private int fontSize;
 
 	public GameObject potato;
-	//float z = 0;
+	float z;
 		//public float speedForce = 1f;
 		//public Vector3 u;
-
+void Start(){
+//if(Input.GetKey(KeyCode.Mouse0)){
+			
+		//}
+}
 
 	void Update () {
+		MovePotato();
+		transform.Translate (z, 0f, 0f);
 		if(Input.GetKey(KeyCode.Mouse0)){
+		z =-50f * Time.deltaTime;
+	}
+		//	transform.Translate (-50f * Time.deltaTime, 0f, 0f);
+			//rigidbody.AddForce(0,10,0);
+
+
 			//rigidbody.velocity = new Vector3 (0,0,-speedForce);
 			//Rigidbody.AddForce(u,0);
 			//z*=5;
-		}
+		//}
 		if(Input.GetKey(KeyCode.Space)){
 			Application.LoadLevel (2);
 		}
+	}
+
+	void MovePotato() {
+		Vector2 mouseMovement = new Vector2(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"));
+
+		Vector3 currentPos = transform.position;
+		currentPos.x += mouseMovement.y * Time.deltaTime * 50;
+		currentPos.z += mouseMovement.x * Time.deltaTime * 50;
+		currentPos.y = 0;
+
+		transform.position = currentPos;
 	}
 	void OnGUI()
 	{
