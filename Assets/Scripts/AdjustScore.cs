0b6ad875b465d76cf227350095de80bbd62a5348
Maja Daniela Hansen <MajaDanielaHansen@Gmail.com>
2015-04-04 11:26:11 +0200

    Changing to highscore scene when time limit is reached.

--- Assets/Scripts/AdjustScore.cs
+++ Assets/Scripts/AdjustScore.cs
@@ -5,6 +5,7 @@ public class AdjustScore : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		/*
 	
 		if (Input.GetKeyDown (KeyCode.UpArrow))
 			MainGameManager.instance.AdjustScore (1);
@@ -17,5 +18,6 @@ public class AdjustScore : MonoBehaviour {
 
 		if (Input.GetKeyDown (KeyCode.D))
 			MainGameManager.instance.StopTimer ();
+			*/
 	}
 }
--- Assets/Scripts/MainGameManager.cs
+++ Assets/Scripts/MainGameManager.cs
@@ -6,18 +6,34 @@ public class MainGameManager : MonoBehaviour {
 	public static MainGameManager instance;
 	private int score;
 	private ArrayList peeledPotatoes = new ArrayList();
-	private float timeRemaining = 60;
+	private float timeRemaining = 6;
 	private IEnumerator countdown;
 	private bool isCountingDown;
 
+	//GUI
+	public Font newFont;
+	public int fontSize;
+
 	void Awake(){
-
-		if (instance == null)
+		/*
+		if(instance) {
+			DestroyImmediate(gameObject);
+		} else {
+			DontDestroyOnLoad(gameObject);
 			instance = this;
-
-		DontDestroyOnLoad (instance);
+		}
+		*/
 		countdown = CountdownTime();
 		isCountingDown = false;
+		StartCoroutine("StartTimer");
+	}
+	void Update(){
+		
+		if (timeRemaining<=5){
+			//Change to highscoreScene
+			StopTimer();
+			Application.LoadLevel (0);
+		}
 	}
 
 	public void AdjustScore(int i){
@@ -42,9 +58,21 @@ public class MainGameManager : MonoBehaviour {
 
 		while (timeRemaining > 0) {
 			timeRemaining = timeRemaining - Time.fixedDeltaTime;
-			Debug.Log("Time remaining: " + timeRemaining);
+			//Debug.Log("Time remaining: " + timeRemaining);
 			yield return new WaitForFixedUpdate();
 		}
 		Debug.Log ("Time's up!");
 	}
+	void OnGUI()
+	{
+		GUIStyle nStyle = new GUIStyle ();
+		nStyle.font = newFont;
+		nStyle.normal.textColor = new Color(100,100,100);
+		//Display time countdown
+		nStyle.fontSize = 100;
+		GUI.TextField (new Rect (Screen.width/2-50, 320, 200, 200), "" + timeRemaining, nStyle);
+		//Display score
+		nStyle.fontSize = 50;
+		GUI.TextField (new Rect (Screen.width/4, 50, 200, 200), "Peeled potatoes: " + score, nStyle);
+	}	
 }
