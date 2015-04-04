d64c57013b04c92a39677d80737397465394e79c
Simon Ingemann Christensen <ingemannofdoom@gmail.com>
2015-04-04 16:51:33 +0200

    merge

--- Assets/Scripts/SimonTest.cs
+++ Assets/Scripts/SimonTest.cs
@@ -5,11 +5,12 @@ public class SimonTest : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		MainGameManager.instance.StopTimer();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKeyDown(KeyCode.D))
+			Debug.Log(MainGameManager.instance.score);
 	}
 }
