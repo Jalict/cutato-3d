2e29e113de2b5d6aaba13745f69f69e2a9b0706d
Maja Daniela Hansen <MajaDanielaHansen@Gmail.com>
2015-04-04 20:24:55 +0200

    Clock movement is done!

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -4,21 +4,26 @@ using System;
 
 public class Clock : MonoBehaviour {
 	public Transform SecondHand;
-	public Transform MinuteHand;
-	public Transform HourHand;
+	
+	float timer = 0;
+    float second = 0;
+
+    //Timer
+	public int countSpeed = 50; //amount of time between spawns
+	private int countNum = 0;
 	
 	void Update () {
-		DateTime currentTime = DateTime.Now;
-		float second = (float) currentTime.Second;
-		float minute = (float) currentTime.Minute;
-		float hour = (float) currentTime.Hour %12;
-
-		float secondAngle = 360 * (second/60);		
-		float minuteAngle = 360 * (minute/60);
-		float hourAngle = 360 * (hour/12);
-
+		if(countNum > countSpeed){
+ 		second+=1.0f;
+ 		countNum = 0;	
+   }
+   if (second>=60)
+       		{
+        		   second = 0;
+       		    timer = 0;
+      		 }
+   countNum++;
+		float secondAngle = -360 * (second/60);		
 		SecondHand.localRotation = Quaternion.Euler(0,0, secondAngle);
-		MinuteHand.localRotation = Quaternion.Euler(0,0, minuteAngle);
-		HourHand.localRotation = Quaternion.Euler(0,0, hourAngle);
 	}
 }
