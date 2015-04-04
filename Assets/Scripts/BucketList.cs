44263edcffc0f48af06fbd73c3ffde1799c0f43c
Maja Daniela Hansen <MajaDanielaHansen@Gmail.com>
2015-04-04 14:52:17 +0200

    Bucket

---
+++ Assets/Scripts/BucketList.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BucketList : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	/*
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+
+	GameObject[] objects = GameObject.FindGameObjectsWithTag("myTag");
+}
+*/
+}
