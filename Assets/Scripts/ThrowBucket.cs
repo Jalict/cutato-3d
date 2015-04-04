402fcc57a61cd45b91f98611de02b8d102d02fd4
Simon Ingemann Christensen <ingemannofdoom@gmail.com>
2015-04-04 23:27:45 +0200

    throwbucket

--- Assets/Scripts/ThrowBucket.cs
+++ Assets/Scripts/ThrowBucket.cs
@@ -6,6 +6,7 @@ public class ThrowBucket : MonoBehaviour {
 	public Transform targetTransform;
 	public float fromLerpSpeed = 5.0f;
 	public float backLerpSpeed = 2.5f;
+	public int numPotatoes = 20;
 	private Vector3 startPos;
 	private Vector3 targetPos;
 	public GameObject[] potatoes = new GameObject[1];
@@ -19,12 +20,18 @@ public class ThrowBucket : MonoBehaviour {
 	}
 	
 	public IEnumerator ThrowPotatoes(){
-
-		while (Vector3.Distance(transform.position, targetPos) > 0.1f) {
+		
+		while (Vector3.Distance(transform.position, targetPos) > 5.0f) {
 			transform.position = Vector3.Lerp (transform.position, targetPos, fromLerpSpeed * Time.fixedDeltaTime);
 			yield return new WaitForFixedUpdate();
 		}
-		while (Vector3.Distance(transform.position, startPos) > 0.1f) {
+
+		for (int i = 0; i < numPotatoes; i++) {
+			Instantiate(potatoes[Random.Range(0, potatoes.Length)], transform.position, Quaternion.identity);
+			yield return new WaitForFixedUpdate();
+		}
+		
+		while (Vector3.Distance(transform.position, startPos) > 1.0f) {
 			transform.position = Vector3.Lerp(transform.position, startPos, backLerpSpeed * Time.fixedDeltaTime);
 			yield return new WaitForFixedUpdate();
 		}
