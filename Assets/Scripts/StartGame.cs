62e64f4e1389065c8d441abf3717bd74071b5402
Maja Daniela Hansen <MajaDanielaHansen@Gmail.com>
2015-04-05 00:05:10 +0200

    Pot prefab. Able to shift from start scene by shooting potato.

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -8,50 +8,38 @@ public class StartGame : MonoBehaviour {
 
 	public GameObject potato;
 	float z;
-		//public float speedForce = 1f;
-		//public Vector3 u;
-void Start(){
-//if(Input.GetKey(KeyCode.Mouse0)){
-			
-		//}
-}
+	private bool isShooted = false;
 
 	void Update () {
 		MovePotato();
 		transform.Translate (z, 0f, 0f);
 		if(Input.GetKey(KeyCode.Mouse0)){
-		z =-50f * Time.deltaTime;
-	}
-		//	transform.Translate (-50f * Time.deltaTime, 0f, 0f);
-			//rigidbody.AddForce(0,10,0);
-
-
-			//rigidbody.velocity = new Vector3 (0,0,-speedForce);
-			//Rigidbody.AddForce(u,0);
-			//z*=5;
-		//}
-		if(Input.GetKey(KeyCode.Space)){
-			Application.LoadLevel (2);
+			z =-50f * Time.deltaTime;
+			isShooted = true;
 		}
 	}
 
 	void MovePotato() {
-		Vector2 mouseMovement = new Vector2(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"));
-
-		Vector3 currentPos = transform.position;
-		currentPos.x += mouseMovement.y * Time.deltaTime * 50;
-		currentPos.z += mouseMovement.x * Time.deltaTime * 50;
-		currentPos.y = 0;
-
-		transform.position = currentPos;
+		if(isShooted == false){ //bool so it does not move after being shoot away.
+			Vector2 mouseMovement = new Vector2(0, Input.GetAxis("Mouse X")); //only able to move right and left.
+			Vector3 currentPos = transform.position;
+			currentPos.x += mouseMovement.y * Time.deltaTime * 50;
+			currentPos.z += mouseMovement.x * Time.deltaTime * 50;
+			currentPos.y = 0;
+			transform.position = currentPos;
+		}
 	}
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Pot"){
+			Application.LoadLevel (2);
+		}
+	}
+
 	void OnGUI()
 	{
 		GUIStyle nStyle = new GUIStyle ();
 		nStyle.font = newFont;
 		nStyle.normal.textColor = new Color(100,100,100);
-		nStyle.fontSize = 18;
-		GUI.TextField (new Rect (Screen.width/2-50, 10, 200, 200), "Start peeling to begin!", nStyle);
 		nStyle.fontSize = 200;
 		GUI.TextField (new Rect (Screen.width/2-50, 320, 200, 200), "Cutato!", nStyle);
 	}	
