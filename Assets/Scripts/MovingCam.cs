a8cd29edcb49732641173b48e889d2d1c1c1e5c5
Maja Daniela Hansen <MajaDanielaHansen@Gmail.com>
2015-04-05 12:57:59 +0200

    Cam stuff. Just ignore

---
+++ Assets/Scripts/MovingCam.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingCam : MonoBehaviour {
+	private float z;
+	private bool isMovingRight = true;
+	
+	void Update () {
+		transform.Translate (z, 0f, 0f);
+		if(Input.GetKeyDown(KeyCode.A)){
+			z =-2f * Time.deltaTime;
+		}
+			if(Input.GetKeyDown(KeyCode.D)){
+			z =+ 2f* Time.deltaTime;
+		}
+			
+	}
+}
