c8224428f08962470c1f1906ec6cbc746c75d2ba
Simon Ingemann Christensen <ingemannofdoom@gmail.com>
2015-04-05 03:33:49 +0200

    merge

---
+++ Assets/Scripts/SimonTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimonTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MainGameManager.instance.StartTimer ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
